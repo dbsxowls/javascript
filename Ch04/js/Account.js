2ae28607a5671e81cc7406f6945020cad909070a
taejinyun <reetok77@gmail.com>
2020-12-08 16:58:36 +0900

    2020/12/08

--- Ch04/js/Account.js
+++ Ch04/js/Account.js
@@ -1,18 +1,26 @@
-function Account(){
+function Account(bank,id,name,money){
      
     //멤버 변수
-    var bank;
-    var id;
-    var name;
-    var money;
-    
+    this.bank = bank;
+    this.id = id;
+    this.name = name;
+    this.money = money;
 
     //멤버메서드
-    var deposit = function(){}
-    var withdraw = function(){}
-    var show = function(){}
+    this.deposit = function(_money){
+        this.money += _money
+    }
+    this.withdraw = function(_money){
+        this.money-=_money;
+    }
+    this.show = function(){
+        document.write('----------------<br />');
+        document.write('은행명 : '+this.bank+'<br />');
+        document.write('계좌번호 : '+this.id+'<br />');
+        document.write('입금주 : '+this.name+'<br />');
+        document.write('현재잔액 : '+this.money+'<br />');
+        document.write('은행명 : '+this.bank+'<br />');
+        document.write('----------------<br />');
+    }
     
-
-
-
 }
