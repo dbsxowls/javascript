85d0c0739f287b81f23c8203f41177cc987b9426
taejinyun <reetok77@gmail.com>
2020-12-08 14:44:11 +0900

    2020/12/08

---
+++ Ch04/js/Account.js
@@ -0,0 +1,18 @@
+function Account(){
+     
+    //멤버 변수
+    var bank;
+    var id;
+    var name;
+    var money;
+    
+
+    //멤버메서드
+    var deposit = function(){}
+    var withdraw = function(){}
+    var show = function(){}
+    
+
+
+
+}
